eafe19cb71ad70c7d0f4872f472702d05f6d37ec
Kovats Kornel <kovats.kornel95@gmail.com>
2022-01-29 21:20:20 +0100

    fix: updated all dependencies

---
+++ src/todos/todos.service.spec.ts
@@ -0,0 +1,10 @@
+import { Test } from '@nestjs/testing';
+import { TodosService } from './todos.service';
+
+describe('TodosService', () => {
+    let todosService: TodosService;
+
+    beforeEach(()=>{
+        //todosService = new TodosService();
+    })
+});
